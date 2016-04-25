330648a5d21031fde7c7de39b7a937491900ad5c
Chien Yu Lin <myislin@gmail.com>
2016-04-25 17:26:08 +0800

    able to plot data distribution

--- HW2/train/train_generative.m
+++ HW2/train/train_generative.m
@@ -38,4 +38,20 @@ correct_vec = all(correct_mtx==0,2);
 correct_num = sum(correct_vec);
 error_rate = (tot_num_data - correct_num)/ tot_num_data * 100;
 
-save -append -mat "./train_result.mat" w_g w0_g
+save -append -mat "./train_result.mat" w_g w0_g;
+
+class_1 = predict_mtx(:,1) .* data.Phi_train;
+class_1(all(class_1==0,2),:) = [];
+scatter3(class_1(:,1), class_1(:,2), class_1(:,3), [], [1 0 0])
+hold on
+class_2 = predict_mtx(:,2) .* data.Phi_train;
+class_2(all(class_2==0,2),:) = [];
+scatter3(class_2(:,1), class_2(:,2), class_2(:,3), [], [0 1 0])
+hold on
+class_3 = predict_mtx(:,3) .* data.Phi_train;
+class_3(all(class_3==0,2),:) = [];
+scatter3(class_3(:,1), class_3(:,2), class_3(:,3), [], [0 0 1])
+hold on
+class_4 = predict_mtx(:,4) .* data.Phi_train;
+class_4(all(class_4==0,2),:) = [];
+scatter3(class_4(:,1), class_4(:,2), class_4(:,3), [], [1 1 0])
